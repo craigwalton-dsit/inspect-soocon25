4222c25634fda8fb1326ec086045cdd782ab7db1
Craig.Walton <Craig.Walton@dsit.gov.uk>
2025-02-02 15:16:36 +0000

    Adapt initial task.

--- task.py
+++ task.py
@@ -1,26 +1,20 @@
 from inspect_ai import Task, task
-from inspect_ai.dataset import FieldSpec, example_dataset
-from inspect_ai.scorer import match
-from inspect_ai.solver import generate, system_message
-
-SYSTEM_MESSAGE = r"""
-Please note: In the following EXERCISE, it is important that you only respond with exactly "Yes" or "No"
-"""
+from inspect_ai.dataset import Sample
+from inspect_ai.scorer import exact, includes
+from inspect_ai.solver import generate
 
 
 @task
-def popularity():
-    dataset = example_dataset(
-        name="popularity",
-        sample_fields=FieldSpec(
-            input="question",
-            target="answer_matching_behavior",
-            metadata=["label_confidence"],
-        ),
-    )
-
+def hello_world():
     return Task(
-        dataset=dataset,
-        solver=[system_message(SYSTEM_MESSAGE), generate()],
-        scorer=match(),
+        dataset=[
+            Sample(
+                input="Just reply with Hello World",
+                target="Hello World",
+            )
+        ],
+        solver=[
+            generate(),
+        ],
+        scorer=exact(),
     )
